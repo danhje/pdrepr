0130b8b94572cc34febc0996e9fdde0ff502adae
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2021-02-10 23:48:54 +0100

    Add first test

--- tests/test_pdrepr.py
+++ tests/test_pdrepr.py
@@ -3,5 +3,7 @@ import pandas as pd
 
 
 def test_pdrepr_minimal():
-    pdrepr(pd.DataFrame())
-    assert True
+    original_df = pd.DataFrame({"col1": [1, 2, 3], "col2": ["num1", "num2", "num3"]})
+    string_representation = pdrepr(original_df)
+    recreated_df = eval(string_representation)
+    pd.testing.assert_frame_equal(original_df, recreated_df)
