79dc3ef6f304aeb7ad86b5d8e5f434a06bcaa036
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2021-02-11 23:33:22 +0100

    First stab at the actual module, passes 5/8 tests

--- pdrepr/_pdrepr.py
+++ pdrepr/_pdrepr.py
@@ -3,8 +3,32 @@ Create DataFrame string representations that can be used to create new DataFrame
 """
 
 
-from pandas import DataFrame
+import pandas as pd
 
 
-def pdrepr(df: DataFrame) -> str:
-    return ""
+ALIAS = "pd"
+
+
+def pdrepr(df: pd.DataFrame) -> str:
+    """Create DataFrame string representation that can be used to create a new DataFrame.
+
+    Args:
+        df: DataFrame to serialize
+
+    Returns:
+        String containing valid Python code that can be pasted into a script file or interpreter,
+        or passed to eval(), thereby creating a DataFrame similar to the original.
+
+    Example:
+        string_repr = pdrepr(my_df)
+        new_df = eval(string_repr)
+    """
+
+    index_columns = df.index.names
+    df = df.reset_index()
+    d = df.to_dict(orient="list")
+    d.pop("index", None)
+    string = f"{ALIAS}.DataFrame({repr(d)})"
+    if sum([idx is not None for idx in index_columns]):
+        string += f".set_index({index_columns})"
+    return string
