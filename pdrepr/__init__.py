8357b0c1a4431123606b01703fe70f63d8db74d6
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2021-02-14 12:22:28 +0100

    Bump patch version.

--- pdrepr/__init__.py
+++ pdrepr/__init__.py
@@ -6,8 +6,4 @@ Create DataFrame string representations that can be used to create new DataFrame
 from ._pdrepr import pdrepr
 
 
-__author__ = 'Daniel Hjertholm'
-__version__ = '0.1.0'
-
-
 __all__ = [pdrepr]
